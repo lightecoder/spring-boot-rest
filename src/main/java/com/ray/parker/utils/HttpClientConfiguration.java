ee145d38f91a75af38fda3b18f7236028f403fa9
Ray_ParkerDEV <4456602@gmail.com>
2019-02-04 13:55:31 +0200

    update

--- src/main/java/com/ray/parker/utils/HttpClientConfiguration.java
+++ src/main/java/com/ray/parker/utils/HttpClientConfiguration.java
@@ -1,12 +1,8 @@
 package com.ray.parker.utils;
 
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.IOException;
 
 @Configuration
 public class HttpClientConfiguration {
@@ -14,15 +10,4 @@ public class HttpClientConfiguration {
     public OkHttpClient httpClient() {
         return new OkHttpClient();
     }
-
-//    @Bean
-//    public Request request() {
-//        return new Request.Builder().url("").build();
-//    }
-//
-//    @Bean
-//    public Response response() throws IOException {
-//        return new OkHttpClient().newCall(new Request.Builder().url("").build()).execute();
-//    }
-
 }
